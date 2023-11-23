e6c3eb1a3e4966f09cb174d2b3307f892bbb7af9
Angelo <ljy6037@gmail.com>
2023-11-23 14:45:32 +0900

    first commit (초기 설정)

---
+++ src/main/java/com/example/prj2be231123/Prj2Be231123Application.java
@@ -0,0 +1,13 @@
+package com.example.prj2be231123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj2Be231123Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj2Be231123Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prj2be231123/Prj2Be231123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj2be231123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj2Be231123ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
