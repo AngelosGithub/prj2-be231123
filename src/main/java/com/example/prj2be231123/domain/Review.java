2958e117bf41dc282e60cde1765a9de23ddf9487
Angelo <ljy6037@gmail.com>
2023-12-06 15:27:28 +0900

    231206 게시물 작성 시간 표기 변경

--- src/main/java/com/example/prj2be231123/domain/Review.java
+++ src/main/java/com/example/prj2be231123/domain/Review.java
@@ -3,7 +3,10 @@ package com.example.prj2be231123.domain;
 import lombok.Data;
 import org.apache.ibatis.annotations.Insert;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 @Data
 public class Review {
@@ -17,4 +20,28 @@ public class Review {
     private LocalDateTime inserted;
     private Integer countComment;
     private Integer starPoint;
+
+    public String getAgo() {
+        LocalDateTime now = LocalDateTime.now();
+
+        if (inserted.isBefore(now.minusYears(1))) {
+            Period between = Period.between(inserted.toLocalDate(), now.toLocalDate());
+            return between.get(ChronoUnit.YEARS) + "년 전";
+        } else if (inserted.isBefore(now.minusMonths(1))) {
+            Period between = Period.between(inserted.toLocalDate(), now.toLocalDate());
+            return between.get(ChronoUnit.MONTHS) + "달 전";
+        } else if (inserted.isBefore(now.minusDays(1))) {
+            Period between = Period.between(inserted.toLocalDate(), now.toLocalDate());
+            return between.get(ChronoUnit.DAYS) + "일 전";
+        } else if (inserted.isBefore(now.minusHours(1))) {
+            Duration between = Duration.between(inserted, now);
+            return (between.getSeconds() / 60 / 60) + "시간 전";
+        } else if (inserted.isBefore(now.minusMinutes(1))) {
+            Duration between = Duration.between(inserted, now);
+            return (between.getSeconds() / 60) + "분 전";
+        } else {
+            Duration between = Duration.between(inserted, now);
+            return between.getSeconds() + "초 전";
+        }
+    }
 }
--- src/main/java/com/example/prj2be231123/service/StarService.java
+++ src/main/java/com/example/prj2be231123/service/StarService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class StarService {
-    private final StarMapper mapper;
+//    private final StarMapper mapper;
 
     public void update(Star star) {
         // 처음 별점을 줄때 : insert
